18ff9b197025180ba7debc28d54688a78cc4e3c5
Alina Cho <alinacho@Alinas-MacBook-Pro.local>
2024-09-21 10:20:40 -0700

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
-import {BrowswerRouter, Rotes, Route} from "react-router-dom"
+import { BrowserRouter ,Routes, Route, Link } from "react-router-dom"
+import { HomePage } from './components/Home/HomePage';
 
 function App() {
+
   return (
     <div className="App">
-        <BrowswerRouter>
-        <Rotes>
-          <Route>
-
-          </Route>
-        </Rotes>
-        </BrowswerRouter>     
+      <BrowserRouter>
+      <Routes>
+          <Route path="/home" element={<HomePage/>}/>
+        </Routes>
+      </BrowserRouter>
+        
     </div>
   );
+}
+
+
+export const Navbar = () => {
+  return (
+    <div>
+        <Link to='/home'>Home</Link>
+        <Link to='/login'>Loign</Link>
+        <Link to='/signup'>SinUp</Link>
+    </div>
+  )
 }
 
 export default App;
---
+++ frontend/src/components/Home/HomePage.js
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+    return (
+        <div>
+            <h2>Hoome page</h2>
+        </div>
+    ) 
+}

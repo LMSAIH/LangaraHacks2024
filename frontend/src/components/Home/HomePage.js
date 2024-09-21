c040e4c6eff6c9f9899d8f2f8465daec215a7b21
Alina Cho <alinacho@Alinas-MacBook-Pro.local>
2024-09-21 11:36:43 -0700

    some changes 2

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useStocksContext } from "../../hooks/useStocksContext"
 import { IncomeForm } from "../Forms/IncomesForm";
+import { StocksContainer } from "./StocksContainer";
 
 export const HomePage = () => {
 
     const {stocks, dispatch} = useStocksContext();
     const [savings, setSavings] = useState(0);
+
+    useEffect(() => {
+
+    }, [])
 
     return (
         <div className="homeWrapper">

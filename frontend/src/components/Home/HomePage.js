42fe4a24b955415df0a3d41bf08cc1907a5eeecf
Axel Velasquez <yoloax11@gmail.com>
2024-09-21 14:52:01 -0700

    made the changes

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -1,7 +1,12 @@
+import { useState, useEffect } from "react";
+import { fetchStocks } from "../../functionalities/fetchWithIncome";
+import axios from "axios";
+
 export const HomePage = () => {
-    return (
-        <div>
-            <h2>Hoome page</h2>
-        </div>
-    ) 
-}
+
+  return (
+    <div>
+      <h2>Hoome page</h2>
+    </div>
+  );
+};

14b4ad41ced42632d3244def101da9eacda4467d
Lana-Cambeses <lanacambeses@gmail.com>
2024-09-21 15:51:31 -0700

    lana changes

--- frontend/src/components/Landing/FeaturesSection.js
+++ frontend/src/components/Landing/FeaturesSection.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import f1 from './images/feat1.png';
+import f2 from './images/feat2.png';
+import f3 from './images/feat3.png';
+import f4 from './images/feat4.png';
 
 const FeaturesSection = () => {
   return (
@@ -6,22 +10,22 @@ const FeaturesSection = () => {
       <h2>Features</h2>
       <div className="features-grid">
         <div className="feature">
-          <img src="icon1.png" alt="Budgeting" />
+          <img src={f1} alt="Budgeting" />
           <h3>Budgeting Made Easy</h3>
           <p>Track your income and expenses effortlessly.</p>
         </div>
         <div className="feature">
-          <img src="icon2.png" alt="Investments" />
+          <img src={f2} alt="Investments" />
           <h3>Personalized Investment Tips</h3>
           <p>Grow your savings with expert recommendations.</p>
         </div>
         <div className="feature">
-          <img src="icon3.png" alt="Savings Goals" />
+          <img src={f3} alt="Savings Goals" />
           <h3>Track Savings Goals</h3>
           <p>Stay on top of your financial goals with real-time insights.</p>
         </div>
         <div className="feature">
-          <img src="icon4.png" alt="Debt Management" />
+          <img src={f4} alt="Debt Management" />
           <h3>Debt Management</h3>
           <p>Manage and pay off debts effectively.</p>
         </div>
--- frontend/src/components/Landing/Nav.js
+++ frontend/src/components/Landing/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from './images/logo2.png';
 
 const Nav = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
+        <img src={logo} height="30px"></img>
         <a href="/">FinanceAdvisor</a>
       </div>
       <div className="navbar-buttons">

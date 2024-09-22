d6a3c16fc50cd66e1d8063330e0d387d15bdf6ef
Axel Velasquez <yoloax11@gmail.com>
2024-09-21 21:20:36 -0700

    made some changes to the server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const { generateMeta } = require('./controllers/aiController');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { generateMeta } = require("./controllers/aiController");
+const UserRoutes = require("./routes/userRoutes");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use((req,res,next) => {
+    console.log(req.method, req.path);
+    next();
+});
 
 app.use(cors());
+app.use("/api/user/", UserRoutes);
+app.post("/api/aiprompt", generateMeta);
 
-app.listen(4000, () => {console.log('Cors enabled web server listening to port 4000')});
-app.post('/api/aiprompt', generateMeta);
+mongoose.connect(process.env.DB_ACCESS)
+.then(() => {
+  app.listen(4000, () => {
+    console.log("Cors enabled web server, connected to db and listening to port 4000");
+  });
+})
+.catch((err) => {console.log(err)});
+
 

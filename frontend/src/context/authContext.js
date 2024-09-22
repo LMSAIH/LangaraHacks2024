9655af506c4f7057978da524556bee2ba7e4892e
Axel Velasquez <yoloax11@gmail.com>
2024-09-21 23:50:54 -0700

    updated the auth context

--- frontend/src/context/authContext.js
+++ frontend/src/context/authContext.js
@@ -3,31 +3,37 @@ import { createContext, useEffect, useReducer } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
-    const authReducer = (state, action) => {
-        switch (action.type) {
-            case "LOGIN" :
-                return {
-                    user: action.payload,
-                };
-            case "LOGOUT" : 
-                return {
-                    user: null
-                };
-            default: return state;
-        }
+  const authReducer = (state, action) => {
+    switch (action.type) {
+      case "LOGIN":
+        return {
+          user: action.payload,
+        };
+      case "LOGOUT":
+        return {
+          user: null,
+        };
+      default:
+        return state;
     }
+  };
 
-    const [state, dispatch] = useReducer(authReducer, {
-        user: null
-    });
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
 
-    useEffect(() => {
+  useEffect(() => {
+    //stored in json so its a json string
+    const user = JSON.parse(localStorage.getItem("user"));
 
-    }, [])
-    
-    return (
-        <AuthContext.Provider value={{...state, dispatch}}>
-            {props.children}
-        </AuthContext.Provider>
-    )
-}
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
